1ea9a1f1e214ec8ce08701ccc6833febda025992
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-06-11 12:41:28 -0700

    create textinput and adding simple styling

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState }from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding:30}}>
+      <TextInput placeholder="Course Goal" style={{borderColor:'black', borderWidth:1, padding:10}}/>
+      <Button title="ADD"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, 
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
