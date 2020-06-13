958d29634e967ee093245e7957015b13cb89fc05
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-06-13 12:21:07 -0700

    added textInput by using React Hooks

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import React, { useState }from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function App() {
- 
-  return (
-    <View style={{padding:30}}>
-      <TextInput placeholder="Course Goal" style={{borderColor:'black', borderWidth:1, padding:10}}/>
-      <Button title="ADD"/>
-    </View>
-  );
-}
+export default function App (){
+  const [text, setText] = useState("");
+
+  const addGoal = () => {
+    console.log(text)
+  }
+    return (
+      <View style={styles.screen}>
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Course Goal" 
+                   style={styles.input}
+                   onChangeText={(text) => setText(text)}
+                   value={text}
+          />
+          <Button title="ADD"
+                  onPress={addGoal}
+          />
+        </View>   
+      </View>
+    );
+ }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen:{
+    padding:30
   },
-});
+  inputContainer:{
+    flexDirection:'row', 
+    justifyContent:'space-between', 
+    alignItems:'center'
+  },
+  input:{
+    borderColor:'black', 
+    borderWidth:1, 
+    padding:10, 
+    width:'80%'
+  }
+})
+
