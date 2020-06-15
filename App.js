363f6ee5d3c5df248304a0174dfe39e64fedb56b
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-06-15 15:33:06 -0700

    create expo-cli react-native app and added Header component

--- App.js
+++ App.js
@@ -1,49 +1,21 @@
-import React, { useState }from 'react';
-import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
-import GoalItem from "./component/GoalItem.js";
-import GoalInput from "./component/GoalInput.js";
+import React from 'react';
+import {View, StyleSheet } from "react-native";
+import Header from "./components/Header"
+
+
 
 export default function App (){
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [modalVisible, setModalVisible] = useState(false)
-
-  const addGoalHandler = (goalTitle) => {
-    setCourseGoals(currentGoals => [...currentGoals, { id:Math.random().toString(), value:goalTitle }
-      ]);
-    setModalVisible(false)
-  };
-
-  const removeGoalHandler = (goalId) => {
-    setCourseGoals(currentGoals => {
-      return currentGoals.filter((goal) => goal.id != goalId);
-    });
-  };
-
-  const cancelGoalAddition = () => {
-    setModalVisible(false)
-  }
   return (
     <View style={styles.screen}>
-      <Button title="Add New Goal" onPress={() => setModalVisible(true)}/>
-      <GoalInput
-        visible={modalVisible}
-        onAddGoal={addGoalHandler}
-        onCancel={cancelGoalAddition}
-      />
-      <FlatList
-        keyExtractor={(item,index) => item.id}
-        data={courseGoals}
-        renderItem={itemData => (
-          <GoalItem onDelete={() => removeGoalHandler(itemData.item.id)} title={itemData.item.value}/>  
-          )}
-      /> 
+      <Header title={'Guess a Number'}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  screen:{
-    padding:30
-  },
+const styles=StyleSheet.create({
+    screen:{
+        flex:1
+    }
 })
+
 
