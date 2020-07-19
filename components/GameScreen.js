e775bddcd8f0d4ffa39e0ab16a717d515a5c2aa2
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-07-19 14:07:35 -0700

    created custom button

--- components/GameScreen.js
+++ components/GameScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { View, Text, StyleSheet, Button, Alert } from "react-native";
 import Card from "./Card";
 import NumberContainer from "./NumberContainer";
+import MainButton from "./MainButton";
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min)
@@ -51,8 +52,8 @@ const GameScreen = props => {
           <Text>Opponent's Guess</Text>
           <NumberContainer>{currentGuess}</NumberContainer>
           <Card style={styles.buttonContainer}>
-            <Button title="LOWER" onPress={nextGuessHandler.bind(this,'lower')}/>
-            <Button title="GREATER" onPress={nextGuessHandler.bind(this,'greater')}/>
+            <MainButton onPress={nextGuessHandler.bind(this,'lower')}>LOWER</MainButton>
+            <MainButton onPress={nextGuessHandler.bind(this,'greater')}>GREATER</MainButton>
           </Card>
         </View>
         
@@ -69,8 +70,8 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         justifyContent:'space-around',
         marginTop:20,
-        width:300,
-        maxWidth:'80%'
+        width:400,
+        maxWidth:'90%'
     }
 
 })
