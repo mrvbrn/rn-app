235f7d0ba3049c667e7bddc6779d9774a04a5473
Merve Baran <46411924+mrvbrn@users.noreply.github.com>
2020-07-20 15:04:24 -0700

    Revert "Head"

--- components/GameScreen.js
+++ components/GameScreen.js
@@ -3,8 +3,6 @@ import { View, Text, StyleSheet, Button, Alert } from "react-native";
 import Card from "./Card";
 import NumberContainer from "./NumberContainer";
 import MainButton from "./MainButton";
-import { Ionicons } from "@expo/vector-icons";
-
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min)
@@ -54,17 +52,8 @@ const GameScreen = props => {
           <Text>Opponent's Guess</Text>
           <NumberContainer>{currentGuess}</NumberContainer>
           <Card style={styles.buttonContainer}>
-
-            <MainButton onPress={nextGuessHandler.bind(this,'lower')}>
-              <Ionicons name="md-remove"/>
-            </MainButton>
-            <MainButton onPress={nextGuessHandler.bind(this,'greater')}>
-              <Ionicons name="md-add"/>
-            </MainButton>
-
             <MainButton onPress={nextGuessHandler.bind(this,'lower')}>LOWER</MainButton>
             <MainButton onPress={nextGuessHandler.bind(this,'greater')}>GREATER</MainButton>
-
           </Card>
         </View>
         
@@ -81,9 +70,6 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         justifyContent:'space-around',
         marginTop:20,
-
-        width:300,
-        maxWidth:'80%',
         width:400,
         maxWidth:'90%'
     }
